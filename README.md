99677de45b717680c79f5dcda45bdca350bc1b61
reskeee <comandos200809@gmail.com@gmail.com>
2025-03-08 16:38:29 +0700

    Working AI module

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Tehnostrelka
+
+## Запуск
+Перед запуском сервера необходимо подготовить виртуальную среду:
+```sh
+python -m venv venv
+
+# Unix
+source venv/bin/activate
+# Windows
+.\venv\Scripts\activate
+
+pip install -r requirements.txt
+```
+
+И инициализировать векторную базу данных:
+```sh
+sudo docker run -p 6333:6333 -p 6334:6334 -v "$(pwd)/qdrant_storage:/qdrant/storage:z" qdrant/qdrant
+```
+
+Запуск сервера:
+```sh
+python manage.py runserver 
+```
+
+
--- ai_module/vec_db.py
+++ ai_module/vec_db.py
@@ -1,36 +1,60 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import VectorParams, Distance
-from ai_module import embed_sentence
+from ai_module import embed_sentence, retriever, reranker_model
 
-# Инициализация клиента Qdrant
 client = QdrantClient(host='127.0.0.1', port=6333)
 
-COLLECTION_NAME = "test_collection"
+COLLECTION_NAME = "assym_search"
 
-def create_collection(collection_name):
+# Функция для создания новой коллекции
+def create_collection(collection_name: str):
     client.recreate_collection(collection_name=collection_name,
                                vectors_config=VectorParams(size=768, distance=Distance.COSINE))
 
-if __name__ == "__main__":
 
-    create_collection(COLLECTION_NAME)
+# Функция поиска ближайшего вектора
+def search_nearest(collection_name: str, text: str):
+    vector = retriever.encode(text)
+    return client.search(collection_name, vector)
 
-    films = [("Название", "описание")]
 
-    for i, payload in enumerate(films):
-        vector = embed_sentence(payload[1])
+# Функция для наполнения базы данных
+def db_filling(collection_name: str):
+    from tqdm import tqdm
+    import pandas as pd
+
+    df = pd.read_csv("films.csv")
+
+    for i, payload in tqdm(df.iterrows()):
+        # vector = embed_sentence(payload.iloc[3])
+        vector = retriever.encode(payload.iloc[3])
         client.upsert(
-            collection_name=COLLECTION_NAME,
+            collection_name=collection_name,
             points=[
                 {
                     'id': i,
                     'vector': vector.tolist(),
                     'payload': {
-                        'title': payload[0],
-                        'about': payload[1]
+                        'title': payload.iloc[0],
+                        'about': payload.iloc[3]
                         }
                 }
             ]
         )
 
-    print("Тексты успешно векторизованы и записаны в Qdrant.")
+
+if __name__ == "__main__":
+    from pprint import pprint
+    
+    # text = "Типы играют в огромные шахматы чтобы забрать философский камень"
+    # text = "школьника укусил паук-мутант, когда они с классом были на экскурсии"
+    text = "Богатый негр ищет себе водителя для поездки по америке, чтобы давать концерты"
+    
+    pprint(type(search_nearest(COLLECTION_NAME, text)[0]))
+
+
+    # create_collection(COLLECTION_NAME)
+    # db_filling(COLLECTION_NAME)
+
+
+
